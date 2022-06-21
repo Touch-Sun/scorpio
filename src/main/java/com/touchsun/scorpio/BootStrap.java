f9ebfd07a173e2071776c0259eace58b3ad70a69
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-21 13:10:20 +0800

    refactor: 响应逻辑采用Response组件重构

--- src/main/java/com/touchsun/scorpio/BootStrap.java
+++ src/main/java/com/touchsun/scorpio/BootStrap.java
@@ -1,15 +1,17 @@
 package com.touchsun.scorpio;
 
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.StrUtil;
 import com.touchsun.scorpio.core.config.ScorpioConfig;
+import com.touchsun.scorpio.core.constant.ResponseConstant;
 import com.touchsun.scorpio.core.web.Request;
+import com.touchsun.scorpio.core.web.Response;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 /**
  * 启动类
@@ -41,25 +43,48 @@ public class BootStrap {
                 // 收到的浏览器的一个请求
                 Socket socket = serverSocket.accept();
 
-                // 实例化Request对象解析Http
+                // 实例化Request对象解析Http,处理输入数据(请求)
                 Request request = new Request(socket);
                 System.out.println(("收到Http请求信息：" + request.getRequestContent()));
                 System.out.println(("请求Uri：" + request.getUri()));
 
+                // 实例化Response对象,处理输出数据(响应)
+                Response response = new Response();
+                String html = "Hi, Welcome to Scorpio!";
+                response.getPrintWriter().println(html);
 
-                // 处理输出数据(响应)
-                OutputStream outputStream = socket.getOutputStream();
-                String responseHead = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n\r\n";
-                String responseContent = "Hi, Welcome to Scorpio!";
-                String response = responseHead + responseContent;
-                outputStream.write(response.getBytes());
-                outputStream.flush();
-                socket.close();
+                reply200(socket,response);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 响应200
+     * @param socket 客户端连接
+     * @param response 响应对象
+     * @throws IOException IO异常
+     */
+    private static void reply200(Socket socket, Response response) throws IOException {
+        // 构建头部信息
+        String header = StrUtil.format(ResponseConstant.RESPONSE_200, ResponseConstant.TEXT_HTML);
+        // 解析头部/身体的字节信息
+        byte[] headerBytes = header.getBytes();
+        byte[] bodyBytes = response.getBody();
+
+        // 构建整体字节信息
+        byte[] responseBytes = new byte[headerBytes.length + bodyBytes.length];
+        // 头部/身体的字节信息拷贝到整体字节信息中 [...headerBytes.....bodyBytes....]
+        ArrayUtil.copy(headerBytes, 0, responseBytes, 0, headerBytes.length);
+        ArrayUtil.copy(bodyBytes, 0, responseBytes, headerBytes.length, bodyBytes.length);
+
+        // 响应数据
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write(responseBytes);
+        outputStream.flush();
+        socket.close();
     }
 }
 
