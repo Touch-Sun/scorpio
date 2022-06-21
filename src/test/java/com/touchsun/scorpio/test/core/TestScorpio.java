7fc8f128784fb4c9a01aa114d1cb1219f3b7ff01
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-21 16:56:06 +0800

    test: 增加文件URI单元测试/更改判断Scorpio存活逻辑

--- src/test/java/com/touchsun/scorpio/test/core/TestScorpio.java
+++ src/test/java/com/touchsun/scorpio/test/core/TestScorpio.java
@@ -1,6 +1,5 @@
 package com.touchsun.scorpio.test.core;
 
-import cn.hutool.core.util.NetUtil;
 import cn.hutool.core.util.StrUtil;
 import com.touchsun.scorpio.core.config.ScorpioConfig;
 import com.touchsun.scorpio.core.plugin.VirtualBrowser;
@@ -17,7 +16,7 @@ public class TestScorpio {
     @BeforeClass
     public static void beforeClass() {
         // 所有的单元测试启动之前，先看Scorpio是否启动了
-        if (NetUtil.isUsableLocalPort(ScorpioConfig.DEFAULT_PORT)) {
+        if (!scorpioIsEnable()) {
             System.err.println("在端口[io-" + ScorpioConfig.DEFAULT_PORT + "]未检测到Scorpio进程，请启动Scorpio后进行测试");
             System.exit(1);
         } else {
@@ -25,10 +24,22 @@ public class TestScorpio {
         }
     }
 
+    /**
+     * 测试路径[/]访问状况
+     */
     @Test
     public void testHelloScorpio() {
         String html = getContent("/");
-        Assert.assertEquals(html, "Hi, Welcome to Scorpio!");
+        Assert.assertEquals(html, ScorpioConfig.MSG_WELCOME);
+    }
+
+    /**
+     * 测试HTML文件[/hello.html]访问状况
+     */
+    @Test
+    public void testHtmlScorpio() {
+        String html = getContent("/hello.html");
+        Assert.assertEquals(html, "Hi,Scorpio,this is html file!");
     }
 
     /**
@@ -40,5 +51,17 @@ public class TestScorpio {
         String url = StrUtil.format("http://{}:{}{}",
                 ScorpioConfig.DEFAULT_ADDRESS, ScorpioConfig.DEFAULT_PORT, uri);
         return VirtualBrowser.getContent(url);
+    }
+
+    /**
+     * 判定Scorpio是否启动
+     * 向Scorpio[/]URI发生请求，返回内容不为空，证明存活
+     * @return 是否启动布尔值
+     */
+    public static boolean scorpioIsEnable() {
+        String url = StrUtil.format("http://{}:{}{}",
+                ScorpioConfig.DEFAULT_ADDRESS, ScorpioConfig.DEFAULT_PORT, "/");
+        String html = VirtualBrowser.getContent(url);
+        return StrUtil.isNotEmpty(html);
     }
 }
