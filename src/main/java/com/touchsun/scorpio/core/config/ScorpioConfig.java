a2623d68cc18b15caef0c9f771be907248a2ab65
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-21 23:44:41 +0800

    feat: 增加log4j日志记录功能

--- src/main/java/com/touchsun/scorpio/core/config/ScorpioConfig.java
+++ src/main/java/com/touchsun/scorpio/core/config/ScorpioConfig.java
@@ -1,5 +1,6 @@
 package com.touchsun.scorpio.core.config;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.system.SystemUtil;
 
 import java.io.File;
@@ -36,6 +37,16 @@ public class ScorpioConfig {
     public static final String MSG_WELCOME = "Hi, Welcome to Scorpio!";
 
     /**
+     * 符号[\t\t]
+     */
+    public static final String SYMBOL_TAB_TAB = "\t\t";
+
+    /**
+     * 符号[:]
+     */
+    public static final String SYMBOL_COLON = ":";
+
+    /**
      * 静态文件路径[WEBAPPS_FOLDER]
      */
     public final static File WEBAPPS_FOLDER = new File(SystemUtil.get("user.dir"), "webapps");
@@ -44,4 +55,58 @@ public class ScorpioConfig {
      * 静态文件路径[ROOT_FOLDER]
      */
     public final static File ROOT_FOLDER = new File(WEBAPPS_FOLDER, "ROOT");
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Scorpio虚拟机信息
+    public static final String JVM_SERVER_VERSION_FILED = "Server Version";
+    public static final String JVM_SERVER_VERSION_VALUE = "Scorpio/0.0.1";
+
+    public static final String JVM_SERVER_BUILD_TIME_FIELD = "Server Build";
+    public static final String JVM_SERVER_BUILD_TIME_VALUE = DateUtil.now();
+
+    public static final String JVM_OS_NAME_FIELD = "OS Name\t";
+    public static final String JVM_OS_NAME_VALUE = SystemUtil.get("os.name");
+
+    public static final String JVM_OS_VERSION_FIELD = "OS Version";
+    public static final String JVM_OS_VERSION_VALUE = SystemUtil.get("os.version");
+
+    public static final String JVM_OS_ARCHITECTURE_FIELD = "OS Architecture";
+    public static final String JVM_OS_ARCHITECTURE_VALUE = SystemUtil.get("os.arch");
+
+    public static final String JVM_JAVA_HOME_FIELD = "Java Home";
+    public static final String JVM_JAVA_HOME_VALUE = SystemUtil.get("java.home");
+
+    public static final String JVM_JAVA_VERSION_FIELD = "Java Version";
+    public static final String JVM_JAVA_VERSION_VALUE = SystemUtil.get("java.version");
+
+    public static final String JVM_JAVA_VENDOR_FIELD = "Java Vendor";
+    public static final String JVM_JAVA_VENDOR_VALUE = SystemUtil.get("java.vm.specification.vendor");
+    ///////////////////////////////////////////////////////////////////////////
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
