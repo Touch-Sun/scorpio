a811adce15c73dd998235722a9de8f250083917b
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-24 16:18:11 +0800

    feat: 增加Scorpio统一异常/异常信息

---
+++ src/main/java/com/touchsun/scorpio/exception/ExceptionMessage.java
@@ -0,0 +1,13 @@
+package com.touchsun.scorpio.exception;
+
+/**
+ * 异常提示信息
+ * @author Lee
+ */
+public class ExceptionMessage {
+
+    /**
+     * 没有指定Web应用的加载策略 [内核异常]
+     */
+    public static final String LOAD_STRATEGY_EXCEPTION = "Does not have a default program loading strategy!";
+}
---
+++ src/main/java/com/touchsun/scorpio/exception/ScorpioNormalException.java
@@ -0,0 +1,12 @@
+package com.touchsun.scorpio.exception;
+
+/**
+ * 常规异常
+ * @author Lee
+ */
+public class ScorpioNormalException extends IllegalAccessException {
+
+    public ScorpioNormalException(String message) {
+        super(message);
+    }
+}
