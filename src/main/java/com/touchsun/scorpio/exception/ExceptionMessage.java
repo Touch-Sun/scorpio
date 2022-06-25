50945e1f0af1ade6a66a01d1de89d31be99d7ddc
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-26 00:01:41 +0800

    feat: 针对[Engine]引擎增加异常信息

--- src/main/java/com/touchsun/scorpio/exception/ExceptionMessage.java
+++ src/main/java/com/touchsun/scorpio/exception/ExceptionMessage.java
@@ -10,4 +10,9 @@ public class ExceptionMessage {
      * 没有指定Web应用的加载策略 [内核异常]
      */
     public static final String LOAD_STRATEGY_EXCEPTION = "Does not have a default program loading strategy!";
+
+    /**
+     * Scorpio引擎节点的配置有误 [内核异常]
+     */
+    public static final String PARSE_ENGINE_EXCEPTION = "Could not resolve Scorpio engine ENGINE because default Host[{}] does not exist!";
 }
