213a9cbb91770152e2b2748f0eedca6f988d44fc
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-26 11:03:16 +0800

    refactor: 重构ScorpioApplication,采用Server实例启动服务器

--- src/main/java/com/touchsun/scorpio/core/app/ScorpioApplication.java
+++ src/main/java/com/touchsun/scorpio/core/app/ScorpioApplication.java
@@ -1,15 +1,10 @@
 package com.touchsun.scorpio.core.app;
 
 import cn.hutool.log.LogFactory;
-import com.touchsun.scorpio.core.plugin.ThreadActuator;
 import com.touchsun.scorpio.exception.ScorpioNormalException;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 /**
- * Scorpio App
+ * Scorpio静态启动类
  * @author Lee
  */
 public class ScorpioApplication {
@@ -21,31 +16,11 @@ public class ScorpioApplication {
      */
     public static void run(Class<?> bootStrapClass, String[] args) {
         try {
-            // 启动Scorpio日志
-            Core.jvmLog();
-
-            // 初始化Scorpio服务
-            Service service = Service.instance();
-
-            // 开启服务器套接字通讯
-            ServerSocket serverSocket = Core.buildConnect();
-
-            // 循环：处理一个socket连接后在处理下一个
-            while (true) {
-                // 收到的浏览器的一个请求
-                Socket socket = serverSocket.accept();
-                // 向线程池提交处理请求任务
-                ThreadActuator.run(() -> {
-                    try {
-                        Core.handleRequest(socket, service);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        LogFactory.get().error(e);
-                    }
-                });
-            }
-
-        } catch (IOException | ScorpioNormalException e) {
+            // 实例化Scorpio服务器
+            Server server = Server.instance();
+            // 启动Scorpio服务器
+            server.start();
+        } catch (ScorpioNormalException e) {
             e.printStackTrace();
             LogFactory.get().error(e);
         }
