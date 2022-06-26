5563d7d45558d8729ca12ef6e1a4a1dc3d3712a4
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-26 11:02:42 +0800

    refactor: 重构Service,使其兼容[Server]

--- src/main/java/com/touchsun/scorpio/core/app/Service.java
+++ src/main/java/com/touchsun/scorpio/core/app/Service.java
@@ -21,14 +21,21 @@ public class Service {
     @Getter
     private Engine engine;
 
-    public Service() throws ScorpioNormalException {
+    /**
+     * Scorpio服务器
+     */
+    private Server server;
+
+    public Service(Server server) throws ScorpioNormalException {
         // 从应用解析器解析[name]
         this.name = AppXMLParser.parseServiceName();
         // 实例化一个Engine引擎
         this.engine = Engine.instance(this);
+        // 赋值Scorpio服务器
+        this.server = server;
     }
 
-    public static Service instance() throws ScorpioNormalException {
-        return new Service();
+    public static Service instance(Server server) throws ScorpioNormalException {
+        return new Service(server);
     }
 }
