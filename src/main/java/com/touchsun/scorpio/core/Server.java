427b9055490563cdde6d8d8142c658c7039a4838
Touch-Sun <mr.jiaqi1204@gmail.com>
2022-06-27 23:06:23 +0800

    refactor: 重构Scorpio服务器,去除对Scorpio服务的形参依赖

--- src/main/java/com/touchsun/scorpio/core/Server.java
+++ src/main/java/com/touchsun/scorpio/core/Server.java
@@ -3,6 +3,8 @@ package com.touchsun.scorpio.core;
 import cn.hutool.log.LogFactory;
 import com.touchsun.scorpio.plugin.ThreadActuator;
 import com.touchsun.scorpio.exception.ScorpioNormalException;
+import com.touchsun.scorpio.web.Request;
+import com.touchsun.scorpio.web.Response;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -53,13 +55,25 @@ public class Server {
             while (true) {
                 // 收到的浏览器的一个请求
                 Socket socket = serverSocket.accept();
+                // 实例化Request对象解析Http,处理输入数据(请求)
+                Request request = new Request(socket, this.service);
+                // 实例化Response对象,处理输出数据(响应)
+                Response response = new Response();
                 // 向线程池提交处理请求任务
                 ThreadActuator.run(() -> {
                     try {
-                        this.core.handleRequest(socket, service);
+                        // 处理请求
+                        this.core.handleRequest(socket, request, response);
                     } catch (IOException | ScorpioNormalException e) {
                         e.printStackTrace();
                         LogFactory.get().error(e);
+                        try {
+                            // 发生异常响应异常
+                            this.core.reply(socket, response, e);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                            LogFactory.get().error(e);
+                        }
                     }
                 });
             }
